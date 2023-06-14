7d84d9a3f3127798d7ab9c4e7839b373698a698e
chriskilelo <chriskilelo@gmail.com>
2023-06-14 08:00:03 +0300

    Cleaned up the goalRoutes.js file by importing the methods from the goals controller and using them on the HTTP request methods.

--- routes/goalRoutes.js
+++ routes/goalRoutes.js
@@ -1,23 +1,18 @@
 const express = require('express')
 const router = express.Router()
-const {getGoals} = require('../controllers/goalController')
+const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
+
 
 // [GET] Retrieve goals
 router.get('/', getGoals)
 
 // [POST] Create a goal
-router.post('/', (req, res) => {
-    res.status(200).json({message:'Create a goal'})
-})
+router.post('/', setGoal)
 
 // [PUT] Update a goal
-router.put('/:id', (req, res) => {
-    res.status(200).json({message:'Update a goal with ID: ' + req.params.id})
-})
+router.put('/:id', updateGoal)
 
 // [DELETE] Delete a goal
-router.delete('/:id', (req, res) => {
-    res.status(200).json({message:'Delete a goal with ID: ' + req.params.id})
-})
+router.delete('/:id', deleteGoal)
 
 module.exports = router
