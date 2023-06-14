68f88c9bed579c9a7522a54c0f21b56c17473f67
chriskilelo <chriskilelo@gmail.com>
2023-06-14 08:53:58 +0300

    Added middleware to server.js to parses JSON requests and URL encoded bodies.

--- server.js
+++ server.js
@@ -4,8 +4,16 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 // This is the port that we want our server to run on. 
 const port = process.env.APP_PORT || 5000
+// Create an Express application
 const app = express()
+// Return a middleware that only parses json and only looks at requests where the Content-Type header matches the type option.
+app.use(express.json())
+// Return a middleware that only parses urlencoded bodies and only looks at requests where the Content-Type header matches the type option
+app.use(express.urlencoded({ extended: false }))
+
+
+
 
 app.use('/api_v1.0/goals', require('./routes/goalRoutes'))
 
-app.listen(port, ()=> console.log('Server started successfully on port ' + port))
+app.listen(port, () => console.log('Server started successfully on port ' + port))
