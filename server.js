04f7af8a9034d2450b145daeb774c119e24ce903
chriskilelo <chriskilelo@gmail.com>
2023-06-22 09:51:41 +0300

    Made the error handling middle ware available globally throughout the application.

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+/**
+ * TO GOD BE THE GLORY
+ */
+
+
 // Require express which is a minimal and flexible Node.js web application framework
 const express = require('express')
 // Require dotenv which loads environment variables from the .env file into process.env
 const dotenv = require('dotenv').config()
+// Require the error handler middleware
+const {errorHandler} = require('./middleware/errorMiddleware')
 // This is the port that we want our server to run on. 
 const port = process.env.APP_PORT || 5000
 // Create an Express application
@@ -10,10 +17,9 @@ const app = express()
 app.use(express.json())
 // Return a middleware that only parses urlencoded bodies and only looks at requests where the Content-Type header matches the type option
 app.use(express.urlencoded({ extended: false }))
-
-
-
-
+// Send all the '/api_v1.0/goals' requests to the 'goalRoutes' file for redirection
 app.use('/api_v1.0/goals', require('./routes/goalRoutes'))
-
+// Make the error handling middleware available through out the application
+app.use(errorHandler)
+// Specify the port where the application shall listen for connections
 app.listen(port, () => console.log('Server started successfully on port ' + port))
