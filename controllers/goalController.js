3d4c96abf17e3cddf62420376c982c8ed065bc31
chriskilelo <chriskilelo@gmail.com>
2023-06-22 09:16:37 +0300

    Minor additions incorporated into the goalRoutes.js and goalController.js files.

--- controllers/goalController.js
+++ controllers/goalController.js
@@ -20,12 +20,20 @@ const getGoals = (req, res) => {
  * @access  Private
  */
 const setGoal = (req, res) => {
+    // Check whether there is any text that has been passed in
+    if (!req.body.text) {
+        // Means NO text found, issue a 400 bad request error
+        res.status(400)
+        // Use the inbuilt error handler to throw an error
+        throw new Error('Please add a text field')
+    }
+
     res.status(200).json({ message: 'Create a goal' })
 }
 
 
 /**
- * @desc    Update a goal saved in the database.
+ * @desc    Update a goal whose ID has been specified
  * @route   PUT /api_v1.0/goals/:id
  * @access  Private
  */
@@ -35,7 +43,7 @@ const updateGoal = (req, res) => {
 
 
 /**
- * @desc    Retrieve all the goals saved in the database.
+ * @desc    Delete the goal whose ID has been specified
  * @route   DELETE /api_v1.0/goals/:id
  * @access  Private
  */
--- routes/goalRoutes.js
+++ routes/goalRoutes.js
@@ -1,3 +1,10 @@
+/**
+ * TO GOD BE ALL THE GLORY
+ * 22-JUN-2023
+ * The file to handle the routes for all goals
+ */
+
+
 const express = require('express')
 const router = express.Router()
 const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
