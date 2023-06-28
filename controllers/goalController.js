8cd7fc5953be6b7f4b287b9ba1d2fd0b3d6341d3
chriskilelo <chriskilelo@gmail.com>
2023-06-28 08:00:26 +0300

    Implemented the asyncHandler() error handling middleware in the goalController.

--- controllers/goalController.js
+++ controllers/goalController.js
@@ -2,24 +2,23 @@
  * TO GOD BE ALL THE GLORY
  * This is the controller for handling GOAL ops
  */
-
+const asyncHandler = require('express-async-handler')
 
 /**
  * @desc    Retrieve all the goals saved in the database.
  * @route   GET /api_v1.0/goals
  * @access  Private
  */
-const getGoals = async (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Get all goals' })
-}
-
+})
 
 /**
  * @desc    Insert a new goal into the database
  * @route   POST /api_v1.0/goals
  * @access  Private
  */
-const setGoal = async (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
     // Check whether there is any text that has been passed in
     if (!req.body.text) {
         // Means NO text found, issue a 400 bad request error
@@ -29,27 +28,25 @@ const setGoal = async (req, res) => {
     }
 
     res.status(200).json({ message: 'Create a goal' })
-}
-
+})
 
 /**
  * @desc    Update a goal whose ID has been specified
  * @route   PUT /api_v1.0/goals/:id
  * @access  Private
  */
-const updateGoal = async (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Update a goal with ID: ' + req.params.id })
-}
-
+})
 
 /**
  * @desc    Delete the goal whose ID has been specified
  * @route   DELETE /api_v1.0/goals/:id
  * @access  Private
  */
-const deleteGoal = async (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Delete a goal with ID: ' + req.params.id })
-}
+})
 
 module.exports = {
     getGoals,
