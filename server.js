69accce1578bc99a8a88ce0e2cc0ed5ea73905d1
chriskilelo <chriskilelo@gmail.com>
2023-07-05 11:55:25 +0300

    Added the connectDB() method to the backend for connecting to the database.

--- server.js
+++ server.js
@@ -2,19 +2,22 @@
  * TO GOD BE THE GLORY
  */
 
-
 // Require express which is a minimal and flexible Node.js web application framework
 const express = require('express')
 // Require colors to enable us to log to the console in different colors
-const colors =  require('colors')
+const colors = require('colors')
 // Require dotenv which loads environment variables from the .env file into process.env
 const dotenv = require('dotenv').config()
 // Require the error handler middleware
-const {errorHandler} = require('./middleware/errorMiddleware')
+const { errorHandler } = require('./middleware/errorMiddleware')
 // This is the port that we want our server to run on. 
 const port = process.env.APP_PORT || 5000
 // Connect to the database as the application is loading
 const connectDB = require('./config/db')
+// Bring in Mongoose to help with database operations through the models
+const { connect } = require('mongoose')
+// Open a connection to the database
+connectDB()
 // Create an Express application
 const app = express()
 // Return a middleware that only parses json and only looks at requests where the Content-Type header matches the type option.
